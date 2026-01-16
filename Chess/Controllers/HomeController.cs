ab32ac364aa8a43942d76db97fc0cc5281f82d0b
Zarion-07 <y1746579@gmail.com>
2026-01-16 21:06:13 +0530

    About

--- Chess/Controllers/HomeController.cs
+++ Chess/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace Chess.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
